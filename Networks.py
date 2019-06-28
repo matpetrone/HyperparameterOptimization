0f13420053872ee7ed6534757c54db57d6195fb2
matpetrone <matteo.petrone@icloud.com>
2019-06-28 16:59:40 +0200

    first

--- Networks.py
+++ Networks.py
@@ -1,4 +1,18 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from tensorboardX import SummaryWriter
 
 
+class Net(nn.Module):
 
+
+    def __init__(self):
+        self.conv1 = nn.Conv2d(3,16,3)
+        self.conv2 = nn.Conv2d(16, 16,3)
+        self.maxP1 = nn.MaxPool2d(2, 2)
+        self.conv3 = nn.Conv2d(16,32,3)
+        self.conv4 = nn.Conv2d(32,32,3)
+        self.maxP2 = nn.MaxPool2d(2,2)
+        self.
 
--- data.py
+++ data.py
@@ -1,23 +1,44 @@
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, sampler
 import torchvision
+import torch
+from torchvision import transforms
+
+
+
+class IndexSampler(sampler.Sampler):
+    """Samples elements sequentially from some offset.
+    Arguments:
+        num_samples: # of desired datapoints
+        start: offset where we should start selecting from
+    """
+    def __init__(self, num_samples, start=0):
+        self.num_samples = num_samples
+        self.start = start
+
+    def __iter__(self):
+        return iter(range(self.start, self.start + self.num_samples))
+
+    def __len__(self):
+        return self.num_samples
+
 
 def getImageNet(val = True):
 
     transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-    trainSize = 10000
+    trainSize = 100000
     if val:
-        trainSize = 7500
-        valSize = 2500
+        trainSize = 75000
+        valSize = 25000
 
-    trainset = torchvision.datasets.ImageNet(root='./data', split='train', download=True, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, shuffle=False, sampler=ChunkSampler(trainSize, 0))
+    trainset = torchvision.datasets.STL10(root='./data', split='train', download=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, shuffle=False, sampler=IndexSampler(trainSize, 0))
 
-    testset = torchvision.datasets.ImageNet(root="./data", split='val', download=True, transform=transform)
+    testset = torchvision.datasets.STL10(root="./data", split='test', download=True, transform=transform)
     testloader = torch.utils.data.DataLoader(testset, shuffle=False)
 
     if val:
-        validationloader = torch.utils.data.DataLoader(trainset, shuffle=False, sampler=ChunkSampler(valSize, trainSize))
+        validationloader = torch.utils.data.DataLoader(trainset, shuffle=False, sampler=IndexSampler(valSize, trainSize))
         return trainloader, validationloader, testloader
 
     return trainloader, testloader
